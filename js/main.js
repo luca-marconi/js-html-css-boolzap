0bb57ecff267153492a8a427af41ca2266cf1c60
Luca <luca-marconi@hotmail.com>
2020-03-05 19:09:37 +0100

    modifica dell icona invia al click per scrivere il messaggio

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
 
-    $('.fa-microphone').click(function() {
-        var messaggioInput = $('#text').val();
-        $('#text').val('');
+    $('.fa-microphone').click(function() { // al click della classe microfono parte la funzione
+        var messaggioInput = $('#text').val(); // assegno alla variabile il valore inserito nell id text (tramite val)
+        $('#text').val(''); // resetto l id text
 
-        var messaggio = $('.template-message-user .message-user').clone();
-        messaggio.find('.testo-messaggio').text(messaggioInput);
+        var messaggio = $('.template-message-user .message-user').clone(); // assegno alla variabile messaggio la copia template a fine codice html
+        messaggio.find('.testo-messaggio').text(messaggioInput); //cerco la classe testo-messaggio e ci scrivo il messaggioInput
         var dt = new Date();
         var time = dt.getHours() + ":" + dt.getMinutes();
-        messaggio.find('.orario-messaggio').text(time);
-        $('.chat-message').append(messaggio);
-        $('.chat-message').animate({scrollTop: $('.chat-message').prop("scrollHeight")}, 500);
+        messaggio.find('.orario-messaggio').text(time); // cerco la classe orario-messaggio e ci scrivo l orario
+        $('.chat-message').append(messaggio); // prendo la variabile messaggio e la incollo a fine chat (con append)
+        $('.chat-message').animate({scrollTop: $('.chat-message').prop("scrollHeight")}, 500); //scrollo automaticamente alla fine alla ricezione di un nuovo messaggio
+        $('i').addClass('fa-microphone');
+        $('i').removeClass('fa-paper-plane');
 
         setTimeout(risposta, 1000);
         function risposta() {
@@ -26,10 +28,8 @@ $(document).ready(function() {
     });
 
     $('#cerca-contatti').keyup(function(event) {
-        console.log('prova');
         var carattereFiltro = $(this).val().toLowerCase();
 
-        console.log(carattereFiltro);
             $('.rubrica-user .contact').each(function() {
                 var nome = $(this).find('.info-rubrica-user h3').text().toLowerCase();
                 if (nome.includes(carattereFiltro)) {
@@ -38,5 +38,10 @@ $(document).ready(function() {
                 $(this).hide();
                 }
         });
+    });
+
+    $('#text').click(function () {
+        $('i').removeClass('fa-microphone');
+        $('i').addClass('fa-paper-plane');
     });
 });
