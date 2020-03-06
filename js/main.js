44c6b05fd0f44bffa891e2818ab15cce3aa764a8
Luca <luca-marconi@hotmail.com>
2020-03-06 17:12:18 +0100

    pulizia codice

--- js/main.js
+++ js/main.js
@@ -1,30 +1,7 @@
 $(document).ready(function() {
 
-    $('.fa-microphone').click(function() { // al click della classe microfono parte la funzione
-        var messaggioInput = $('#text').val(); // assegno alla variabile il valore inserito nell id text (tramite val)
-        $('#text').val(''); // resetto l id text
-
-        var messaggio = $('.template-message-user .message-user').clone(); // assegno alla variabile messaggio la copia template a fine codice html
-        messaggio.find('.testo-messaggio').text(messaggioInput); //cerco la classe testo-messaggio e ci scrivo il messaggioInput
-        var dt = new Date();
-        var time = dt.getHours() + ":" + dt.getMinutes();
-        messaggio.find('.orario-messaggio').text(time); // cerco la classe orario-messaggio e ci scrivo l orario
-        $('.chat-message').append(messaggio); // prendo la variabile messaggio e la incollo a fine chat (con append)
-        $('.chat-message').animate({scrollTop: $('.chat-message').prop("scrollHeight")}, 500); //scrollo automaticamente alla fine alla ricezione di un nuovo messaggio
-        $('.input-message i:last').addClass('fa-microphone');
-        $('.input-message i:last').removeClass('fa-paper-plane'); 
-
-        setTimeout(risposta, 1000);
-        function risposta() {
-            var messaggioOutput = $('#text').val();
-            $('#text').val('');
-
-            var messaggio = $('.template-message-contact .message-contact').clone();
-            messaggio.find('.testo-messaggio').text('ok');
-            messaggio.find('.orario-messaggio').text(time);
-            $('.chat-message').append(messaggio);
-            $('.chat-message').animate({scrollTop: $('.chat-message').prop("scrollHeight")}, 500);
-        }
+    $('.fa-microphone').click(function() {
+        invioMessaggio();
     });
 
     $('#cerca-contatti').keyup(function(event) {
@@ -44,4 +21,44 @@ $(document).ready(function() {
         $('i').removeClass('fa-microphone');
         $('.input-message i:last').addClass('fa-paper-plane');
     });
+
+    $('.input-message').keypress(function(event) {
+        if (event.which == 13) {
+            invioMessaggio()
+        }
+    });
+
+
+    function invioMessaggio() { // funzione per invio messaggio
+        var messaggioInput = $('#text').val();
+        if (messaggioInput.trim().length > 0) {
+            $('#text').val('');
+            var messaggio = $('.template-message-user .message-user').clone();
+            messaggio.find('.testo-messaggio').text(messaggioInput);
+            var dt = new Date();
+            var time = dt.getHours() + ":" + dt.getMinutes();
+            messaggio.find('.orario-messaggio').text(time);
+            $('.chat-message').append(messaggio);
+            $('.chat-message').animate({scrollTop: $('.chat-message').prop("scrollHeight")}, 500);
+            $('.input-message i:last').addClass('fa-microphone');
+            $('.input-message i:last').removeClass('fa-paper-plane');
+            setTimeout(function() {
+                invioRisposta();
+            }, 1000);
+
+        }
+    }
+    function invioRisposta() {// funzione per invio automatico della risposta
+
+        var messaggioOutput = $('#text').val();
+        $('#text').val('');
+        var messaggio = $('.template-message-contact .message-contact').clone();
+        messaggio.find('.testo-messaggio').text('ok');
+        var dt = new Date();
+        var time = dt.getHours() + ":" + dt.getMinutes();
+        messaggio.find('.orario-messaggio').text(time);
+        $('.chat-message').append(messaggio);
+        $('.chat-message').animate({scrollTop: $('.chat-message').prop("scrollHeight")}, 500);
+    }
+
 });
